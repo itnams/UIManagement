9f4adece2bdb79df28beaf3419ed600e7922e5db
ERM <erm@ERMs-MacBook-Pro.local>
2021-10-27 08:58:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uimanagement/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.uimanagement
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.uimanagement.ui.theme.UIManagementTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            UIManagementTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    UIManagementTheme {
+        Greeting("Android")
+    }
+}
